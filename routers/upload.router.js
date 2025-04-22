e11c7af1235a15d86ee0556305f0f22315cc914f
Ian <ianfarina9@gmail.com>
2025-04-22 10:48:21 -0300

    conexion del router con el controlador (upload)

--- routers/upload.router.js
+++ routers/upload.router.js
@@ -1,5 +1,6 @@
 import express from 'express'
 const routerUpload = express.Router()
+import controller from '../controllers/upload.controller.js'
 
 routerUpload.post('/', controller.uploadImagen)
 
